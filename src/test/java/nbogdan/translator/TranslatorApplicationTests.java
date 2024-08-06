889edf79269181b247ef741fb54571f55d3583bc
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2024-08-07 00:07:42 +0300

    Simple tests

--- src/test/java/nbogdan/translator/TranslatorApplicationTests.java
+++ src/test/java/nbogdan/translator/TranslatorApplicationTests.java
@@ -1,13 +1,37 @@
 package nbogdan.translator;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TranslatorApplicationTests {
 
-	@Test
+	private final TranslatorController controller;
+	private final RestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
+    TranslatorApplicationTests(@Autowired final TranslatorController controller,
+							   @Autowired final RestTemplateBuilder builder) {
+        this.controller = controller;
+        this.restTemplate = builder.build();
+    }
+
+    @Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
+	@Test
+	void translateOneWord() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/?source={source}&target={target}&query={query}",
+				String.class, "en", "ru", "Hello")).contains("Привет");
+	}
 }
